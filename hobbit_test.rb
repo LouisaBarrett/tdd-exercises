b970690b774ecb6fa1e937e453374ceb9736d0d9
Will Mitchell <wvmitchell@gmail.com>
2013-10-04 22:49:26 -0600

    Added pirate test

--- hobbit_test.rb
+++ hobbit_test.rb
@@ -1,6 +1,7 @@
 gem 'minitest'
 require 'minitest/autorun'
 require 'minitest/pride'
+require_relative '../lib/hobbit'
 
 class HobbitTest < Minitest::Test
   def test_it_has_a_name
@@ -9,25 +10,21 @@ class HobbitTest < Minitest::Test
   end
 
   def test_it_is_named_something_else
-    skip
     hobbit = Hobbit.new("Pippin")
     assert_equal "Pippin", hobbit.name
   end
 
   def test_disposition_is_unadventurous
-    skip
     hobbit = Hobbit.new("Samwise")
     assert_equal "homebody", hobbit.disposition
   end
 
   def test_can_have_a_different_disposition
-    skip
     hobbit = Hobbit.new("Frodo", "adventurous")
     assert_equal "adventurous", hobbit.disposition
   end
 
   def test_grows_older_when_celebrating_birthdays
-    skip
     hobbit = Hobbit.new('Merry')
     assert_equal 0, hobbit.age
     5.times do
@@ -37,7 +34,6 @@ class HobbitTest < Minitest::Test
   end
 
   def test_is_considered_a_child_at_32
-    skip
     hobbit = Hobbit.new('Gerontius')
     32.times do
       hobbit.celebrate_birthday
@@ -46,7 +42,6 @@ class HobbitTest < Minitest::Test
   end
 
   def test_comes_of_age_at_33
-    skip
     hobbit = Hobbit.new('Otho')
     33.times do
       hobbit.celebrate_birthday
